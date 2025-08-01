0f219485345be58d61f9708bb80f98800afdfc80
Zhuoyang Liu <144525970+ZhuoyangLiu2005@users.noreply.github.com>
2025-08-01 20:40:08 +0800

    add bimanual joint

--- vla/datasets/rlds/oxe/mixtures.py
+++ vla/datasets/rlds/oxe/mixtures.py
@@ -11,26 +11,23 @@ from typing import Dict, List, Tuple
 OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
     # === Bridge V2 Dataset ===
     "bridge": [
-        # ("bridge_oxe", 1.0),                                    # Version of Bridge V2 in Open-X GCP Bucket
         ("bridge_orig", 1.0),                                   # Original Version of Bridge V2 from Project Website
     ],
     "rlbench": [
-         # ("bridge_oxe", 1.0),                                    # Version of Bridge V2 in Open-X GCP Bucket
-         ("rlbench", 1.0),                                   # Original Version of Bridge V2 from Project Website
+         ("rlbench", 1.0),                            
     ],
     "rlbench_bi": [
-         # ("bridge_oxe", 1.0),                                    # Version of Bridge V2 in Open-X GCP Bucket
-         ("rlbench_bi", 1.0),                                   # Original Version of Bridge V2 from Project Website
+         ("rlbench_bi", 1.0),                                 
+    ],
+    "rlbench_bi_joint": [
+         ("rlbench_bi_joint", 1.0),                                  
     ],
     "rtx_dataset": [
-         # ("bridge_oxe", 1.0),                                    # Version of Bridge V2 in Open-X GCP Bucket
          ("rtx_dataset", 1.0),                                   # Original Version of Bridge V2 from Project Website
     ],
     # === [Moderate-Scale] Bridge++ Mixtures ===
     "bridge_rt_1": [
-        # ("bridge_oxe", 1.0)                                   # Version of Bridge V2 in Open-X GCP Bucket
         ("bridge_orig", 1.0),                                   # Original Version of Bridge V2 from Project Website
-
         ("fractal20220817_data", 1.0),                          # Google RT-1 Robot Data (Large-Scale)
     ],
 
